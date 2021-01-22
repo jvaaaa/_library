34547ad2c68b91339e06c7068ac23af72bc70bce
jvaaaa <1035197980@qq.com>
2021-01-22 22:51:15 +0800

    first commit

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-import com._LibrarySystem.www.view.Menu;
-
-public class Main {
-    public static void main(String[] args){
-        new Menu();
-    }
-}
--- src/exp1/Main.java
+++
@@ -1,9 +0,0 @@
-package exp1;
-
-import exp1.service.Login;
-
-public class Main {
-    public static void main(String[] args){
-        new Login();
-    }
-}
